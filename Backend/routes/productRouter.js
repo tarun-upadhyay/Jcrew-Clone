5e7c485c67de5f69c2f946a31e89fa9cf3d2f19f
tarun-upadhyay <tarunu88@gmail.com>
2022-12-15 22:36:47 +0530

    backend

--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -4,8 +4,19 @@ const productRouter = express.Router();
 const { WomenModel, MenModel } = require("../models/productModel");
 
 productRouter.get("/women", async (req, res) => {
-  const params = req.query;
-
+  const {productID} = req.params;
+  const params = req.query
+  //console.log(params)
+  console.log(productID,10)
+if(productID){
+  try {
+    const products = await WomenModel.find({_id:params});
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+    res.send({ err: "something went wrong" });
+  }
+}else{
   try {
     const products = await WomenModel.find(params);
     res.send(products);
@@ -13,6 +24,10 @@ productRouter.get("/women", async (req, res) => {
     console.log(err);
     res.send({ err: "something went wrong" });
   }
+}
+  
+
+
 });
 productRouter.post("/women", async (req, res) => {
   const payload = req.body;
