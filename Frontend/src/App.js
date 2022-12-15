dcd8c18c930b15843f8482250a253f8647152ad0
Vishal <vishaldas6566@gmail.com>
2022-12-16 02:13:56 +0530

    added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,11 +3,13 @@ import Footer from './Components/Footer/Footer'
 import Mainroute from './Components/Mainroute/Mainroute'
 import Navbar from './Components/Navbar/Navbar'
 import Signin from './Pages/Signin/Signin'
+import Signup from './Pages/Signup/Signup'
 
 const App = () => {
   return (
    <>
    <Signin />
+   {/* <Signup /> */}
    </>
   )
 }
