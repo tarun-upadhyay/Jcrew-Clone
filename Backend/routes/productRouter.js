2c610c457a44969237eedce6bd5a2ab236ef3bce
tarun-upadhyay <tarunu88@gmail.com>
2022-12-15 18:13:34 +0530

    backend

---
+++ Backend/routes/productRouter.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const productRouter = express.Router();
+
+const { WomenModel, MenModel } = require("../models/productModel");
+
+productRouter.get("/women", async (req, res) => {
+  const params = req.query;
+
+  try {
+    const products = await WomenModel.find(params);
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+    res.send({ err: "something went wrong" });
+  }
+});
+productRouter.post("/women", async (req, res) => {
+  const payload = req.body;
+  const product = new WomenModel(payload);
+  await product.save();
+  res.send({ msg: "Added Succeffully" });
+});
+
+productRouter.get("/men", async (req, res) => {
+  const params = req.query;
+
+  try {
+    const products = await MenModel.find(params);
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+    res.send({ err: "something went wrong" });
+  }
+});
+productRouter.post("/men", async (req, res) => {
+  const payload = req.body;
+  const product = new MenModel(payload);
+  await product.save();
+  res.send({ msg: "Added Succeffully" });
+});
+
+module.exports = { productRouter };
