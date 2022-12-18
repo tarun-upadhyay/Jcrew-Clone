b7abada2dc76ff9e7ce13318e107757f81fa89b3
chinmaydonkar <chinmaydonkar@gmail.com>
2022-12-18 22:11:52 +0530

    ress

--- Frontend/src/Components/Products/ProductCard.jsx
+++ Frontend/src/Components/Products/ProductCard.jsx
@@ -1,4 +1,5 @@
-import Button from 'react-bootstrap/Button';
+// import {ButtonH as Button} from 'react-bootstrap/Button';
+import { Button, ButtonGroup } from '@chakra-ui/react';
 import Card from 'react-bootstrap/Card';
 
 function ProductCard ({img,title,desc,originalPrice,size,salePrice}) {
@@ -12,7 +13,7 @@ function ProductCard ({img,title,desc,originalPrice,size,salePrice}) {
         <p style={{width:"100%"}}>Price : <span style={{textDecoration:"line-through"}}>INR {originalPrice}</span> <span style={{color:"red"}}>INR {salePrice}</span></p>
         {/* <p style={{width:"100%"}}>Size : {size && size[0]}</p> */}
         <br/> <br/>
-        <Button variant="primary" style={{width:"100%",right:"0",bottom:"0"}}>Buy</Button>
+        <Button colorScheme='blue' mt='-10' style={{width:"100%"}}>Buy</Button>
       </Card.Body>
     </Card>
     
