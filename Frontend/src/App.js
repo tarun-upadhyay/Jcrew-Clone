3c38ae1cc1922b350de4cae564ef51364847f476
ramkrishnap43 <ramkrishnap.prabhu43@gmail.com>
2022-12-18 21:16:23 +0530

    update

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import Footer from "./Components/Footer/Footer";
-import Footer1 from "./Components/Footer/Footer1";
-import Mainroute from "./Components/Mainroute/Mainroute";
-import Megha from "./Components/Meghamenu/Megha";
-import Navbar from "./Components/Navbar/Navbar";
-import Homepage from "./Pages/Homepage/Homepage";
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Homepage />
-      <br />
-      <Footer1 />
-      <br />
-      <Footer />
-      <br />
-    </div>
-  );
-};
-=======
 import React from 'react'
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -31,17 +9,23 @@ import Signin from './Pages/Signin/Signin'
 import Signup from './Pages/Signup/Signup'
 import ProductsPage from "./Components/Products/ProductsPage"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from './Pages/Homepage/Homepage';
+import Footer1 from './Components/Footer/Footer1';
 // import {Routes,Route} from 'react-router-dom';
 
 const App = () => {
   return (
    <>
+    <Navbar />
+    <Homepage />
+    <Footer1 />
+    <Footer />
    <Signin /> 
     <Signup />
-   <ProductsPage/>
+   <ProductsPage/> 
    </>
   )
 }
->>>>>>> 9632c63c80e51adb3b00347acabbeda5888bb63f
+
 
 export default App;
