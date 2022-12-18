7a714ec0981c187f91d9578144ca82a499ed2870
tarun-upadhyay <tarunu88@gmail.com>
2022-12-18 14:40:06 +0530

    admin

---
+++ Frontend/src/Pages/Admin/Components/Widget/Widget.jsx
@@ -0,0 +1,101 @@
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+
+const Widget = ({ type }) => {
+  let data;
+
+  //temporary
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlinedIcon
+            className="aicon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className="aicon"
+            style={{
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+              color: "goldenrod",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="aicon"
+            style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className="aicon"
+            style={{
+              backgroundColor: "rgba(128, 0, 128, 0.2)",
+              color: "purple",
+            }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div className="awidget">
+      <div className="aleft">
+        <span className="atitle">{data.title}</span>
+        <span className="acounter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <span className="alink">{data.link}</span>
+      </div>
+      <div className="aright">
+        <div className="apercentage apositive">
+          <KeyboardArrowUpIcon />
+          {diff} %
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
--- Frontend/src/Pages/Admin/Pages/home/Home.jsx
+++ Frontend/src/Pages/Admin/Pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Chart from '../../Components/charts/Chart'
+import Featured from '../../Components/featured/Featured'
 import Nav_admin from '../../Components/Navbar/Nav_admin'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Widget from '../../Components/Widget/Widget'
 import "./home.scss"
 const Home = () => {
   return (
@@ -8,7 +11,16 @@ const Home = () => {
        <Sidebar/>
        <div className="homeContainer">
        <Nav_admin/>
-       homeContainer
+       <div className="widgets">
+        <Widget type={"user"}/>
+        <Widget type={"order"}/>
+        <Widget type={"earning"}/>
+        <Widget type={"balance"}/>
+       </div>
+       <div className="charts">
+        <Featured/>
+        <Chart/>
+       </div>
        </div>
     </div>
   )
